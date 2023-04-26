72a15b5fe7f43b1c82b0e77600855ccc42fa3c04
LinaEcker <taylor.ecker@students.tbs1.com>
2023-04-26 14:08:08 +0200

    Datum 14.3.2022

--- database/api/website_collecting.py
+++ database/api/website_collecting.py
@@ -10,7 +10,7 @@ class DataCollector:
     # Constructor - setzte als default das Jahr starten von gestern
     def __init__(self):
         self.end_date = date.today() - relativedelta(days=1)
-        self.start_date = date(2022, 4, 13)
+        self.start_date = date(2022, 3, 14)
 
         self.delta = self.end_date - self.start_date   # returns timedelta
 
